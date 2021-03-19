6948317e2eaf9243c7060e319701f8cf018c470e
Vermium Sifell <vermium@zyner.org>
2021-03-19 18:19:53 +0100

    ✨feature: add school

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -3,8 +3,10 @@ var api = express.Router();
 
 const authentication = require("./authentication");
 const user = require("./user");
+const school = require("./school");
 
 api.use("/authentication", authentication);
 api.use("/user", user);
+api.use("/school", school);
 
 module.exports = api;
