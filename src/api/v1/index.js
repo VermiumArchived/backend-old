36f96cf2ebda49482ff3d3e3a80c530c738eea55
Vermium Sifell <vermium@zyner.org>
2021-03-19 16:50:34 +0100

    ✨feature: add user

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -2,7 +2,9 @@ var express = require("express");
 var api = express.Router();
 
 const authentication = require("./authentication");
+const user = require("./user");
 
 api.use("/authentication", authentication);
+api.use("/user", user);
 
 module.exports = api;
