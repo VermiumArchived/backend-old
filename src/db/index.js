2911576ff75df915768f0be5f70ff07e81780449
Vermium Sifell <vermium@zyner.org>
2021-03-19 18:22:11 +0100

    ✨feature: add index.js

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+const dbConfig = require("./config.js");
+const db = require("./models");
+
+function dbConnection() {
+  db.mongoose
+    .connect(
+      `mongodb+srv://${dbConfig.USERNAME}:${dbConfig.PASSWORD}@${dbConfig.HOST}/${dbConfig.DB}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
+    .then(() => {
+      console.log("Successfully connect to MongoDB.");
+    })
+    .catch((err) => {
+      console.error("Connection error", err);
+      process.exit();
+    });
+}
+
+module.exports = dbConnection;
