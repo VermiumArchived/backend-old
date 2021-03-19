e3416851b01da2cd4ee8670013cdf9b6e1e6813d
Vermium Sifell <vermium@zyner.org>
2021-03-19 14:57:38 +0100

    ✨feature: add index.js

--- src/api/v1/authentication/index.js
+++ src/api/v1/authentication/index.js
@@ -3,32 +3,16 @@ const bodyParser = require("body-parser");
 
 const api = express.Router();
 
-const dbConfig = require("./config/db");
 const initial = require("./initial");
+const dbConnection = require("./dbConnection");
 
 // parse requests of content-type - application/json
 api.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 api.use(bodyParser.urlencoded({ extended: true }));
-const db = require("./models");
 
-db.mongoose
-  .connect(
-    `mongodb+srv://${dbConfig.USERNAME}:${dbConfig.PASSWORD}@${dbConfig.HOST}/${dbConfig.DB}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => {
-    console.log("Successfully connect to MongoDB.");
-    initial();
-  })
-  .catch((err) => {
-    console.error("Connection error", err);
-    process.exit();
-  });
+dbConnection();
 
 require("./routes/auth")(api);
 require("./routes/user")(api);
