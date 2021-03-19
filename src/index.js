22511d2a3528e08daad745260570933f5a980825
Vermium Sifell <vermium@zyner.org>
2021-03-19 13:39:33 +0100

    ✨feature: add version to api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var app = express();
 
-app.use("/api/authentication", require("./api/authentication"));
+app.use("/api/v1/authentication", require("./api/v1/authentication"));
 
 app.get("/", function (req, res) {
   res.send("Hello from root route.");
