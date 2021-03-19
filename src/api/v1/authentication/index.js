dd6b7468f1067bb2ee41f24f86357c873665b333
Vermium Sifell <vermium@zyner.org>
2021-03-19 14:02:12 +0100

    ✨feature: change index.js

--- src/api/v1/authentication/index.js
+++ src/api/v1/authentication/index.js
@@ -2,12 +2,57 @@ var express = require("express");
 
 var api = express.Router();
 
-api.get("/", function (req, res) {
-  res.send("Hello from APIv1 root route.");
-});
+const db = require("./models");
+const Role = db.role;
 
-api.get("/users", function (req, res) {
-  res.send("List of APIv1 users.");
-});
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
 
 module.exports = api;
